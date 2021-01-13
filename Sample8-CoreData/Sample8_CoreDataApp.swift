9c0525e6b89a8827a05c59cfb2b215a1ef86fc3a
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-01-13 16:54:48 +0900

    Initial Commit

---
+++ Sample8-CoreData/Sample8_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample8_CoreDataApp.swift
+//  Sample8-CoreData
+//
+//  Created by keiji yamaki on 2021/01/13.
+//
+
+import SwiftUI
+
+@main
+struct Sample8_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
